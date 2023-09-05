a9b1d3d6e7c0bf6496e6cb04877b3d811506e288
Prisca C <prisca.castanier@gmail.com>
2023-09-06 01:21:23 +0200

    switch to SSR

--- resources/js/Components/NavLink.tsx
+++ resources/js/Components/NavLink.tsx
@@ -1,18 +1,29 @@
 import { Link, InertiaLinkProps } from '@inertiajs/react';
+import { useEffect, useState } from 'react';
 
-export default function NavLink({ active = false, className = '', children, ...props }: InertiaLinkProps & { active: boolean }) {
-    return (
-        <Link
-            {...props}
-            className={
-                'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
-                (active
-                    ? 'border-indigo-400 text-gray-900 focus:border-indigo-700 '
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 ') +
-                className
-            }
-        >
-            {children}
-        </Link>
-    );
+export default function NavLink({
+  active = false,
+  children,
+  ...props
+}: InertiaLinkProps & { active: boolean }) {
+  const [className, setClassName] = useState('');
+
+  useEffect(() => {
+    if (active) {
+      setClassName('border-indigo-500 text-gray-900');
+    } else {
+      setClassName(
+        'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 focus:text-gray-700 focus:border-gray-300',
+      );
+    }
+  }, []);
+
+  const base =
+    'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none';
+
+  return (
+    <Link {...props} className={`${base} ${className}`}>
+      {children}
+    </Link>
+  );
 }
--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,21 +1,22 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
-
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) => {
+    const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true });
+    return pages[`./Pages/${name}.tsx`];
+  },
+  setup({ el, App, props }) {
+    hydrateRoot(el, <App {...props} />);
+  },
+  progress: {
+    color: '#4B5563',
+  },
 });
