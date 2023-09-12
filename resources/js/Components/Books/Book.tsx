f60f0cb313c6c65b4b568858b0416cf3e3138e4d
Prisca C <prisca.castanier@gmail.com>
2023-09-12 14:10:09 +0200

    add condition to avoid page crash on front

--- resources/js/Components/Books/Book.tsx
+++ resources/js/Components/Books/Book.tsx
@@ -2,30 +2,32 @@ import { BookType } from '@/types/BookType';
 
 export const Book = ({ book }: { book: BookType }) => {
   return (
-    <div className="flex flex-col text-center p-2">
-      <article
-        className={
-          'h-[fit-content] md:h-[170px] overflow-hidden flex justify-center items-end'
-        }
-      >
-        <p className={'text-xl'}>
-          {book.title}
-          <br />
-          <span className={'text-sm italic'}>{book.published_at}</span>
-          <br />
-          by <b>{book.author!.name}</b>
-          <br />
-          <span className={'text-sm'}>Publisher: {book.publisher!.name}</span>
-        </p>
-      </article>
-      <div
-        className={
-          'h-[100px] w-[100px] md:h-[150px] md:w-[150px] m-auto flex justify-center items-center'
-        }
-      >
-        <img src={book.cover} alt={book.title} className={'m-auto'} />
+    book && (
+      <div className="flex flex-col text-center p-2">
+        <article
+          className={
+            'h-[fit-content] md:h-[170px] overflow-hidden flex justify-center items-end'
+          }
+        >
+          <p className={'text-xl'}>
+            {book.title}
+            <br />
+            <span className={'text-sm italic'}>{book.published_at}</span>
+            <br />
+            by <b>{book.author!.name}</b>
+            <br />
+            <span className={'text-sm'}>Publisher: {book.publisher!.name}</span>
+          </p>
+        </article>
+        <div
+          className={
+            'h-[100px] w-[100px] md:h-[150px] md:w-[150px] m-auto flex justify-center items-center'
+          }
+        >
+          <img src={book.cover} alt={book.title} className={'m-auto'} />
+        </div>
+        <p className={'text-sm h-[100px] overflow-hidden'}>{book.synopsis}</p>
       </div>
-      <p className={'text-sm h-[100px] overflow-hidden'}>{book.synopsis}</p>
-    </div>
+    )
   );
 };
