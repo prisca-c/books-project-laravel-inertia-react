5fc0a576cab28b16258fdb75c0fd7dbc6112813e
Prisca C <prisca.castanier@gmail.com>
2023-09-15 16:45:24 +0200

    feat(author/publisher): complete crud for author and publisher

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -11,4 +11,33 @@ class AuthorController extends Controller
     {
         return Author::all();
     }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'biography' => 'nullable|string',
+        ]);
+
+        Author::create($request->all());
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'biography' => 'nullable|string',
+        ]);
+
+        $author = Author::findOrFail($id);
+
+        $author->update($request->all());
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $author = Author::findOrFail($id);
+
+        $author->delete();
+    }
 }
