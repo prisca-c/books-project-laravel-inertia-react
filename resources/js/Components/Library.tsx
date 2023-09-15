5a8ba936000138f35db57ba6e54ab6e0dcca8755
Prisca C <prisca.castanier@gmail.com>
2023-09-15 11:34:36 +0200

    feat(rating): added possibility to modify and create a book rating from library view

--- resources/js/Components/Library.tsx
+++ resources/js/Components/Library.tsx
@@ -1,18 +1,32 @@
+import { useState } from 'react';
 import { Book } from '@/Components/Books/Book';
-import type { LibraryType } from '@/types/LibraryType';
 import { toDateNomalized } from '@/Helpers/dateHelpers';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import RatingModal from '@/Components/Modals/RatingModal';
+import type { LibraryType } from '@/types/LibraryType';
 
 type LibraryProps = {
   library: LibraryType;
 };
 
 const Library = ({ library }: LibraryProps) => {
+  const [showRatingModal, setShowRatingModal] = useState(false);
+
   return (
     <div
       className={
         'flex flex-col md:flex-row gap-6 justify-center items-center text-center p-2'
       }
     >
+      {showRatingModal && (
+        <RatingModal
+          setShow={setShowRatingModal}
+          bookId={library.edition?.book?.id!}
+          exist={!!library.rating}
+          userRating={library.rating}
+        />
+      )}
       <div className={'absolute top-0 left-0 p-2 bg-gray-100 rounded-br-lg'}>
         Added to library on {toDateNomalized(library.created_at)}
       </div>
@@ -20,6 +34,16 @@ const Library = ({ library }: LibraryProps) => {
         <Book book={library.edition?.book!} />
       </div>
       <div className={'flex flex-col text-center p-2 gap-6 md:w-[65%]'}>
+        <button
+          onClick={() => setShowRatingModal(true)}
+          className={
+            'text-start bg-gray-100 p-2 rounded-lg w-[fit-content] hover:bg-gray-200'
+          }
+        >
+          <FontAwesomeIcon icon={faStar} className={'text-yellow-500 mr-2'} />
+          {library.rating ? <> Modify my rating </> : <> Rate this book </>}
+        </button>
+
         <p className={'text-xl text-start'}>
           <span className={'font-bold underline'}>Book Edition :</span>{' '}
           {library.edition?.format} - {library.edition?.published_at}
--- resources/js/Components/Modals/RatingModal.tsx
+++ resources/js/Components/Modals/RatingModal.tsx
@@ -4,33 +4,62 @@ import StarRating from '@/Components/RateStars';
 import InputError from '@/Components/InputError';
 import FormButtons from '@/Components/FormButtons';
 import type { BookType } from '@/types/BookType';
+import { RatingType } from '@/types/RatingType';
 
 type RatingModalProps = {
   setShow: React.Dispatch<React.SetStateAction<boolean>>;
   bookId: number;
+  exist?: boolean;
+  userRating?: RatingType | null;
 };
 
-const RatingModal = ({ bookId, setShow }: RatingModalProps) => {
+const RatingModal = ({
+  bookId,
+  setShow,
+  exist = false,
+  userRating,
+}: RatingModalProps) => {
   const [rating, setRating] = useState(1);
-  const { data, setData, post, processing, errors, reset } = useFormRating();
+  const [review, setReview] = useState('');
+  const { data, setData, post, put, processing, errors, reset } =
+    useFormRating();
+
+  useEffect(() => {
+    setRating(userRating?.rating ?? 1);
+    setReview(userRating?.review ?? '');
+    setData({
+      book_id: bookId,
+      rating: rating,
+      review: review,
+      id: exist ? userRating?.id : 0,
+    });
+  }, []);
 
   useEffect(() => {
     setData({
       book_id: bookId,
       rating: rating,
-      review: '',
+      review: review,
+      id: exist ? userRating?.id : 0,
     });
   }, [rating]);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    post(route('dashboard.ratings.store'), {
-      preserveScroll: true,
-      onSuccess: () => {
-        setShow(false);
-      },
-    });
+    exist
+      ? put(route('dashboard.ratings.update', userRating?.id!), {
+          preserveScroll: true,
+          onSuccess: () => {
+            setShow(false);
+          },
+        })
+      : post(route('dashboard.ratings.store'), {
+          preserveScroll: true,
+          onSuccess: () => {
+            setShow(false);
+          },
+        });
   };
 
   const handleStarClick = (newRating: number) => {
@@ -41,7 +70,9 @@ const RatingModal = ({ bookId, setShow }: RatingModalProps) => {
     <div className="fixed z-10 top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
       <form onSubmit={onSubmit} className={'relative bg-white p-4 rounded-lg'}>
         <div className="flex flex-col items-center gap-4 py-4 px-8">
-          <p className="text-center text-2xl font-semibold">Review this book</p>
+          <p className="text-center text-2xl font-semibold">
+            {exist ? 'Modify my rating' : 'Rate this book'}
+          </p>
           <div className="m-auto">
             <StarRating rating={rating} onStarClick={handleStarClick} />
           </div>
